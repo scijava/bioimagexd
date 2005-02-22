831e64bc0a06e95b56b61a2a43bf58d37c75b996
kalpaha <kalpaha@b2d525f8-dbc2-4032-a3a1-b86af67c733b>
2005-02-22 13:15:46 +0000

    Integrating Animator and Urmas
    git-svn-id: http://svn.code.sf.net/p/bioimagexd/code/home/svnuser/repos/selli/trunk@35 b2d525f8-dbc2-4032-a3a1-b86af67c733b

--- LSM/RenderingInterface.py
+++ LSM/RenderingInterface.py
@@ -10,16 +10,17 @@
  interface between LSM and MayaVi
 
  Modified: 17.11.2004 KP - Created the class.
-           13.12.2004 KP -  Added code for previewing
-
+           13.12.2004 KP - Added code for previewing
+           22.02.2005 KP - Added methods necessary to be compatible with Heikki's animator
+           22.02.2005 KP - Introduced concept of current dataset
+           
  Selli includes the following persons:
  JH - Juha Hyytiäinen, juhyytia@st.jyu.fi
  JM - Jaakko Mäntymaa, jahemant@cc.jyu.fi
  KP - Kalle Pahajoki, kalpaha@st.jyu.fi
  JV - Jukka Varsaluoma, varsa@st.jyu.fi
 
- Copyright (c) 2004 Selli Project.
- --------------------------------------------------------------
+ Copyright (c) 2004 Selli Project
 """
 
 __author__ = "Selli Project <http://sovellusprojektit.it.jyu.fi/selli/>"
@@ -44,21 +45,15 @@ def getRenderingInterface():
 
 class RenderingInterface:
     """
-    --------------------------------------------------------------
     Class: RenderingInterface
-    Created: 17.11.2004
-    Creator: KP
+    Created: 17.11.2004, KP
     Description: The interface between LSM and MayaVi for rendering
-    --------------------------------------------------------------
     """
     def __init__(self,dataUnit=None,timePoints=[],**kws):
         """
-        --------------------------------------------------------------
         Method: __init__
-        Created: 17.11.2004
-        Creator: KP
+        Created: 17.11.2004, KP
         Description: Initialization
-        -------------------------------------------------------------
         """
         self.dataUnit=dataUnit
         self.timePoints=timePoints
@@ -68,46 +63,81 @@ class RenderingInterface:
         self.surfcolor=None
         self.settings_mode=0
         self.stop=0
+        self.currentTimePoint=-1
+        # XXX: Make this configurable
+        self.type="png"
+        
+    def getCurrentData(self):
+        if not self.currentData:
+            n=self.currentTimePoint
+            if n<0:n=0
+            self.setCurrentTimePoint(n)
+        return self.currentData
+        
+    def setCurrentTimepoint(self,n):
+        """
+        Method: setCurrentTimepoint(n)
+        Created: 22.02.2005, KP
+        Description: Sets the current timepoint to be the specified timepoint.
+                     This will also update relevant information about the dataset
+        """        
+        self.currentTimePoint = n
+        self.currentData = self.dataUnit.getTimePoint(timepoint)
+        self.dimensions = currentData.GetDimensions()
+        
+    def getRenderWindow(self):
+        """
+        Method: getRenderWindow()
+        Created: 22.02.2005, KP
+        Description: Returns the mayavi's render window. Added for Animator compatibility
+        """        
+        return self.mayavi.get_render_window()
+        
+    def render(self):
+        return self.mayavi.Render()
         
     def runTkinterGUI(self,run=1):
+        """
+        Method: runTkinterGUI()
+        Created: 15.01.2005, KP
+        Description: Executes a Tkinter event loop using wxWidget's wxFutureCall()
+                     that run every 100 ms. Will cease to execute when the flag
+                     self.stop is set
+        """ 
         if self.mayavi:
             self.mayavi.root.update()
         if not self.stop:
             wxPython.wx.wxFutureCall(100, self.runTkinterGUI)
 
 
-
     def setDataUnit(self,dataUnit):
         """
-        --------------------------------------------------------------
         Method: setDataUnit(dataUnit)
-        Created: 17.11.2004
-        Creator: KP
+        Created: 17.11.2004, KP
         Description: Set the dataunit from which the rendered datasets are read
-        -------------------------------------------------------------
         """
         self.dataUnit=dataUnit
+        # Calculate how many digits there will be in the rendered output
+        # file names, with a running counter
+        ndigits=1+int(math.log(self.dataUnit.getLength(),10))
+        # Format, the format will be /path/to/data/image_001.png
+        self.format="%%s%s%%s_%%.%dd.png"%(os.path.sep,ndigits)
+        Logging.info("File name format=",self.format)
 
     def setTimePoints(self,timepoints):
         """
-        --------------------------------------------------------------
         Method: setTimePoints(timepoints)
-        Created: 17.11.2004
-        Creator: KP
+        Created: 17.11.2004, KP
         Description: Set the list of timepoints to be rendered
-        -------------------------------------------------------------
         """
         self.timePoints=timepoints
 
     def createVisualization(self,filename):
         """
-        --------------------------------------------------------------
         Method: createVisualization()
-        Created: 14.12.2004
-        Creator: KP
+        Created: 14.12.2004, KP
         Description: Method for starting Mayavi with a datapoint for the
                      purpose of creating a .mv file to use in rendering.
-        -------------------------------------------------------------
         """
         self.stop=0
         self.runTkinterGUI()
@@ -124,13 +154,10 @@ class RenderingInterface:
 
     def isMayaviRunning(self):
         """
-        --------------------------------------------------------------
         Method: isMayaviRunning()
-        Created: 11.1.2005
-        Creator: KP
+        Created: 11.1.2005, KP
         Description: A method that returns true if a mayavi window exists that 
                      can be used for rendering
-        -------------------------------------------------------------
         """
         Logging.info("Is mayavi running: self.mayavi=",self.mayavi)
         if self.mayavi:
@@ -138,25 +165,64 @@ class RenderingInterface:
                 self.mayavi.root.winfo_exists())
         return (self.mayavi and self.mayavi.root.winfo_exists())
 
+    def getModuleManager(self):
+        """
+        Method: getModuleManager()
+        Created: 22.02.2005, KP
+        Description: A method that returns a current module manager in MayaVi.
+                     If more than one module manager exists, it will return the
+                     "last" one.
+        """
+        dvms = self.mayavi.get_dvm_names()
+        if not self.mm:
+            # Get the last module manager
+            for i in dvms:
+                mm = i.get_current_module_mgr()
+                if self.mm and self.mm != mm:
+                    Logging.warning("Discarding module manager",self.mm)
+                self.mm=mm
+        return self.mm
+
+        
+    def isMayaViModuleLoaded(self):
+        """
+        Method: isMayaviModuleLoaded()
+        Created: 22.02.2005, KP
+        Description: A method that returns true if a mayavi has a visualization module loaded.
+        """
+        mm=self.getModuleManager()
+        if not mm.get_module(0):
+            return 0
+        return 1
+        
+    def createMayaVi(self):
+        """
+        Method: createMayaVi()
+        Created: 22.02.2005, KP
+        Description: A method that creates an instance of mayavi
+        """    
+        Logging.info("Creating new mayavi instance")
+        del self.mayavi
+        self.mayavi=mayavi.mayavi()
+        # set flag indicating this is the first run
+        self.not_loaded=1
+
         
     def doRendering(self,**kws):
         """
-        --------------------------------------------------------------
         Method: doRendering()
-        Created: 17.11.2004
-        Creator: KP
+        Created: 17.11.2004, KP
         Description: Sends each timepoint one at a time to be rendered in mayavi
         Parameters:
             preview     If this flag is true, the results are not rendered out
-        -------------------------------------------------------------
         """
-        self.preview=None
+        self.showPreview=None
         self.stop=0
         self.runTkinterGUI()
 
         self.visualizationFile=None
         self.ctf=None
-        self.ctrl_panel=0
+        self.showControlPanel=0
         self.use_existing=0
         if kws.has_key("use_existing"):
             self.use_existing=kws["use_existing"]
@@ -164,13 +230,12 @@ class RenderingInterface:
         if kws.has_key("module"):
             self.module=kws["module"]
         if kws.has_key("preview"):
-            self.ctrl_panel=1
-            self.preview=kws["preview"]
+            self.showControlPanel=1
+            self.showPreview=kws["preview"]
         if kws.has_key("ctrl_panel"):
-            self.ctrl_panel=kws["ctrl_panel"]
+            self.showControlPanel=kws["ctrl_panel"]
         if kws.has_key("visualization"):
             self.visualizationFile=kws["visualization"]
-
         if kws.has_key("surface_color"):
             self.surfcolor=kws["surface_color"]
         if kws.has_key("ctf"):
@@ -181,66 +246,46 @@ class RenderingInterface:
 
         # If there is no mayavi instance to do the rendering
         # create one
-        if not self.mayavi or not self.mayavi.root.winfo_exists():
-            Logging.info("Creating new mayavi instance")
-            del self.mayavi
-            self.mayavi=mayavi.mayavi()
-            # set flag indicating this is the first run
-            self.not_loaded=1
+        if self.isMayaviRunning() == False:
+            self.createMayaVi()
             
-        Logging.info("Mayavi exists:",self.mayavi.root.winfo_exists())
-        Logging.info("Mayavi state=",self.mayavi.root.state())
+        Logging.info("Mayavi exists:",self.mayavi.root.winfo_exists(), "it's state:",self.mayvia.root.state())
 
         # If this is not preview, we disable the control panel
-        if not self.ctrl_panel:
+        if not self.showControlPanel:
             self.mayavi.show_ctrl_panel(0)
 
-        # Calculate how many digits there will be in the rendered output
-        # file names, with a running counter
-        ndigits=1+int(math.log(self.dataUnit.getLength(),10))
-        # Format, the format will be /path/to/data/image_001.png
-        self.format="%%s%s%%s_%%.%dd.png"%(os.path.sep,ndigits)
-        Logging.info("File name format=",self.format)
-
-        self.framename=self.dataUnit.getName()
+        self.frameName=self.dataUnit.getName()
 
         # If this is not preview
-        if not self.preview:
+        if not self.showPreview:
             # Render every selected timepoint
             for timepoint in self.timePoints:
-                data=self.dataUnit.getTimePoint(timepoint)
-                self.renderData(data,timepoint)
+                # Set the current dataset to be timepoint 
+                self.setCurrentTimePoint(timepoint)
+                self.renderData(self.currentData,timepoint)
 
         else:
             Logging.info("Previewing data")
-            self.renderData(self.preview,0)
+            self.renderData(self.showPreview,0)
         self.stop=1
 
-    def setPath(self,path):
+    def setOutputPath(self,path):
         """
-        --------------------------------------------------------------
         Method: setPath(path)
-        Created: 17.11.2004
-        Creator: KP
+        Created: 17.11.2004, KP
         Description: Sets the path where the rendered frames are stored.
-        -------------------------------------------------------------
         """
         self.dirname=path
-        
-    def getRenderWindow(self):
-        return self.mayavi.get_render_window()
-
+ 
     def renderData(self,data,n):
         """
-        --------------------------------------------------------------
         Method: renderData(data)
-        Created: 17.11.2004
-        Creator: KP
+        Created: 17.11.2004, KP
         Description: Sends the specified timepoint to Mayavi to be rendered.
         Parameters:
                 data               The dataset to be rendered
-                n                   The timepoint
-        -------------------------------------------------------------
+                n                  The timepoint
         """
 #        self.mayavi.close_all()
         if not self.visualizationFile and self.not_loaded:
@@ -275,16 +320,7 @@ class RenderingInterface:
             self.not_loaded=0
 
         # substitute just the data
-        Logging.info("Substituting data...")
-        dvms = self.mayavi.get_dvm_names()
-        for i in dvms:
-            Logging.info("Substituting data to dvm %s"%i)
-            dvm=self.mayavi.mayavi.data_viz_mgr[i]
-            ds=dvm.get_data_source()
-            ds.data=data
-            Logging.info("Updating datasource")
-            ds.Update()
-            ds.update_references()
+        self.setDataSet(data)
         Logging.info("lifting the window")
 
         self.mayavi.root.lift()
@@ -309,9 +345,75 @@ class RenderingInterface:
             self.mayavi.root.lift()
             self.mayavi.root.focus_force()
 
-        if not self.preview:
-            filename=self.format%(self.dirname,self.framename,n)
+        if not self.showPreview:
+            filename=self.getFilename(n)
             print "Rendering timepoint %d with filename %s"%(n,filename)
-            renwin=self.getRenderWindow()
-            renwin.save_png(filename)
+            self.saveFrame(filename)
+            
+    def saveFrame(self,filename):
+        """
+        Method: saveFrame(filename)
+        Created: 22.02.2005, KP
+        Description: Saves a frame with a given name
+        """
+        renwin=self.getRenderWindow()
+        type=self.type
+        comm = "renwin.save_%s(file)"
+        eval(eval("comm%type"))      
+        
+            
+    def getFilename(self,frameNum):
+        """
+        Method: getFilename()
+        Created: 22.02.2005, KP
+        Description: Returns output filename of the frame we're rendering
+        """
+        return self.format%(self.dirname,self.frameName,frameNum)
 
+    def getCenter(self,tp=-1):
+        """
+        Method: getCenter()
+        Created: 22.02.2005, KP
+        Description: Returns the center of the requested dataset. If none is specified, the
+                     center of the current dataset is returned
+        """
+        if self.currentTimePoint<0 or not self.timePoints or tp>max(self.timePoints):
+            return (0,0,0)
+        if tp<0:
+            return self.currentData.GetCenter()
+        else:
+            return self.dataUnit.getTimePoint(tp).GetCenter()
+        
+    def getDimensions(self,tp=-1):
+        """
+        Method: getDimensions(timepoint)
+        Created: 22.02.2005, KP
+        Description: Returns the dimensions of the requested dataset. If none is specified, the
+                     dimensions of the current dataset is returned
+        """    
+        if self.currentTimePoint<0 or not self.timePoints or tp>max(self.timePoints):
+            return (0,0,0)
+        if tp<0:
+            return self.dimensions
+        else:
+            return self.dataUnit.getTimePoint(tp).GetDimensions()
+            
+    def setDataSet(self,dataset):
+        """
+        Method: setDataSet(dataset)
+        Created: 22.02.2005, KP
+        Description: Sets the dataset that is rendered in MayaVi                
+        """
+        dvms = self.mayavi.get_dvm_names()
+        if len(dvms)> 1:
+            Logging.info("There is more than one datavizmgr")
+        for i in dvms:
+            self.mm = i.get_current_module_mgr()
+            Logging.info("Substituting data to dvm %s"%i)
+            dvm=self.mayavi.mayavi.data_viz_mgr[i]
+            ds=dvm.get_data_source()
+            ds.data=dataset
+            self.mayavi.update_label ()
+            Logging.info("Updating datasource")
+            ds.Update()
+            ds.update_references()
