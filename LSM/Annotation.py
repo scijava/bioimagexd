8e20c9fb7727c836c52612110dca5b0c53683072
kalpaha <kalpaha@b2d525f8-dbc2-4032-a3a1-b86af67c733b>
2005-07-03 16:01:46 +0000

    Added a module for annoation objects
    git-svn-id: http://svn.code.sf.net/p/bioimagexd/code/home/svnuser/repos/selli/trunk@324 b2d525f8-dbc2-4032-a3a1-b86af67c733b

---
+++ LSM/Annotation.py
@@ -0,0 +1,31 @@
+# -*- coding: iso-8859-1 -*-
+
+"""
+ Unit: Annotations
+ Project: BioImageXD
+ Created: 10.02.2005, KP
+ Description:
+
+ This is a module that contains classes for annotating images
+ 
+ Copyright (C) 2005  BioImageXD Project
+ See CREDITS.txt for details
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+"""
+
+__author__ = "BioImageXD Project <http://www.bioimagexd.org/>"
+__version__ = "$Revision: 1.21 $"
+__date__ = "$Date: 2005/01/13 13:42:03 $"
