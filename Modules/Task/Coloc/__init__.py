6910ed3dfaa0770a34a55db248f2c72dfdc0c129
kalpaha <kalpaha@b2d525f8-dbc2-4032-a3a1-b86af67c733b>
2005-07-14 08:11:55 +0000

    Coloc
    git-svn-id: http://svn.code.sf.net/p/bioimagexd/code/home/svnuser/repos/selli/trunk@354 b2d525f8-dbc2-4032-a3a1-b86af67c733b

---
+++ Modules/Task/Coloc/__init__.py
@@ -0,0 +1,13 @@
+import Colocalization
+from ColocalizationSettings import *
+from ColocalizationPanel import *
+from DataUnit import CombinedDataUnit
+
+class ColocalizationDataUnit(CombinedDataUnit):
+        def getSettingsClass(self): return ColocalizationSettings
+            
+def getClass(): return Colocalization.Colocalization
+def getConfigPanel(): return ColocalizationPanel
+def getName(): return "Colocalization"
+def getDataUnit(): return ColocalizationDataUnit
+def getSettingsClass(): return ColocalizationSettings
