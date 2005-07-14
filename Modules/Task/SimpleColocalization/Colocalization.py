93466ea303c72aa7e5a3162683936a99a2f7cea4
kalpaha <kalpaha@b2d525f8-dbc2-4032-a3a1-b86af67c733b>
2005-07-14 08:09:18 +0000

    Changes to the colocalization to use the new vtkImageAutoThresholdColocalization and vtkImageColocalizationTest
    git-svn-id: http://svn.code.sf.net/p/bioimagexd/code/home/svnuser/repos/selli/trunk@351 b2d525f8-dbc2-4032-a3a1-b86af67c733b

---
+++ Modules/Task/SimpleColocalization/Colocalization.py
@@ -0,0 +1,158 @@
+# -*- coding: iso-8859-1 -*-
+"""
+ Unit: Colocalization.py
+ Project: BioImageXD
+ Created: 16.11.2004, KP
+ Description:
+
+ Calculates the colocalization between two channels
+ 
+ Copyright (C) 2005  BioImageXD Project
+ See CREDITS.txt for details
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""
+
+__author__ = "Selli Project <http://sovellusprojektit.it.jyu.fi/selli/>"
+__version__ = "$Revision: 1.24 $"
+__date__ = "$Date: 2005/01/13 13:42:03 $"
+
+
+import vtk
+import time
+from enthought.tvtk import messenger
+from Module import *
+
+class Colocalization(Module):
+    """
+    Class: Colocalization
+    Created: 03.11.2004, KP
+    Description: Creates a colocalization map
+    """
+    def __init__(self,**kws):
+        """
+        Method: __init__(**keywords)
+        Created: 03.11.2004, KP
+        Description: Initialization
+        """
+        Module.__init__(self,**kws)
+
+        # TODO: remove attributes that already exist in base class!
+        self.images=[]
+        self.doRGB=0
+        self.x,self.y,self.z=0,0,0
+        self.extent=None
+        self.running=0
+        self.depth=8
+        self.reset()
+
+    def reset(self):
+        """
+        Method: reset()
+        Created: 04.11.2004, KP
+        Description: Resets the module to initial state. This method is
+                     used mainly when doing previews, when the parameters
+                     that control the colocalization are changed and the
+                     preview data becomes invalid.
+        """
+        Module.reset(self)
+        self.colocFilter=vtk.vtkImageColocalizationFilter()
+        self.colocFilter.AddObserver("ProgressEvent",self.updateProgress)
+        self.colocAutoThreshold=vtk.vtkImageAutoThresholdColocalization()
+        self.colocAutoThreshold.AddObserver("ProgressEvent",self.updateProgress)
+        self.thresholds=[]
+        self.settingsLst=[]
+        self.preview=None
+        self.n=-1
+    
+    def addInput(self,dataunit,data):
+        """
+        Method: addInput(data)
+        Created: 03.11.2004, KP
+        Description: Adds an input for the colocalization filter
+        """
+        
+        Module.addInput(self,dataunit,data)
+        settings = dataunit.getSettings()
+        self.settingsLst.append(settings)
+        th0=settings.get("ColocalizationLowerThreshold")
+        th1=settings.get("ColocalizationUpperThreshold")
+        self.thresholds.append((th0,th1))
+        self.depth = self.settings.get("ColocalizationDepth")
+        
+ 
+    def getPreview(self,z):
+        """
+        Method: getPreview(z)
+        Created: 03.11.2004, KP
+        Description: Does a preview calculation for the x-y plane at depth z
+        """
+        if not self.preview:
+            self.preview=self.doOperation()
+        return self.zoomDataset(self.preview)
+
+    def doOperation(self):
+        """
+        Method: doOperation
+        Created: 10.11.2004, KP
+        Description: Does colocalization for the whole dataset
+                     using doColocalizationXBit() where X is user defined
+        """        
+        
+        Logging.info("Doing ",self.depth,"-bit colocalization",kw="processing")
+        self.colocFilter.SetOutputDepth(self.depth)
+
+        
+        settings = self.settingsLst[0]
+        if settings.get("CalculateThresholds"):
+            self.eventDesc="Calculating thresholds"
+            Logging.info("Calculating thresholds...",kw="processing")
+            
+            self.colocAutoThreshold.AddInput(self.images[0])
+            self.colocAutoThreshold.AddInput(self.images[1])
+            self.colocAutoThreshold.Update()
+            Logging.info("Done!",kw="processing")
+            for i in ["Ch1ThresholdMax","Ch2ThresholdMax","PearsonImageAbove",
+                      "PearsonImageBelow","PearsonWholeImage","M1","M2",
+                      "ThresholdM1","ThresholdM2","Slope","Intercept",
+                      "ColocAmount","ColocPercent","PercentageVolumeCh1",
+                      "PercentageVolumeCh2","PercentageMaterialCh1","PercentageMaterialCh2",
+                      "SumOverThresholdCh1","SumOverThresholdCh2","SumCh1","SumCh2"]:
+                method="self.colocAutoThreshold.Get%s()"%i
+                
+                val=eval(method)
+                settings.set(i,val)
+            t1=settings.get("Ch1ThresholdMax")
+            t2=settings.get("Ch2ThresholdMax")
+            l=[(t1,255),(t2,255)]
+            for i in range(0,2):
+                self.settingsLst[i].set("ColocalizationLowerThreshold",l[i][0])
+                self.settingsLst[i].set("ColocalizationUpperThreshold",l[i][1])
+            Logging.info("Threshold for Ch1 =",t1," and Ch2 =",t2,kw="processing")
+            self.thresholds=[(t1,255),(t2,255)]
+            self.settings.set("CalculateThresholds",0)
+            #settings.set("ColocalizationScatterplot",self.colocFilter.GetOutput(1))
+        self.eventDesc="Calculating colocalization..."
+        for i in range(len(self.images)):
+            Logging.info("Using %d as lower and %d as upper threshold"%self.thresholds[i],kw="processing")
+            self.colocFilter.AddInput(self.images[i])
+            self.colocFilter.SetColocalizationLowerThreshold(i,int(self.thresholds[i][0]))
+            self.colocFilter.SetColocalizationUpperThreshold(i,int(self.thresholds[i][1]))
+        self.colocFilter.Update()        
+
+        messenger.send(None,"update_progress",100,"Done.")
+        
+        return self.colocFilter.GetOutput()
