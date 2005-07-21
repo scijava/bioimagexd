8cd193ace072da76f8b2cc7e2aa8c0f442057ac3
kalpaha <kalpaha@b2d525f8-dbc2-4032-a3a1-b86af67c733b>
2005-07-21 16:48:58 +0000

    Added ui as a logging class
    git-svn-id: http://svn.code.sf.net/p/bioimagexd/code/home/svnuser/repos/selli/trunk@392 b2d525f8-dbc2-4032-a3a1-b86af67c733b

--- Logging.py
+++ Logging.py
@@ -44,7 +44,7 @@ HIDE_DEBUG=["visualizer","init","io","scale","preview","trivial",
             "modules","!imageop"]
 KWS=["visualizer","main","init","animator","io","task","preview","scale",
      "imageop","modules","trivial","ctf","dataunit","event","processing",
-     "datasource","iactivepanel","annotation"]
+     "datasource","iactivepanel","annotation","ui"]
 
 import sys
 
