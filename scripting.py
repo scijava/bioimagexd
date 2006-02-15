0b396467a37537b41c5ebb5e27d151edbf4a3b15
kalpaha <kalpaha@b2d525f8-dbc2-4032-a3a1-b86af67c733b>
2006-02-15 10:44:17 +0000

    A module for implementing simple APIs for scripting purposes
    git-svn-id: http://svn.code.sf.net/p/bioimagexd/code/home/svnuser/repos/selli/trunk@581 b2d525f8-dbc2-4032-a3a1-b86af67c733b

---
+++ scripting.py
@@ -0,0 +1,37 @@
+# -*- coding: iso-8859-1 -*-
+
+"""
+ Unit: scripting
+ Project: BioImageXD
+ Created: 13.02.2006, KP
+ Description:
+
+ A module containing various shortcuts for ease of scripting the app
+ 
+ Copyright (C) 2006  BioImageXD Project
+ See CREDITS.txt for details
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+"""
+
+__author__ = "BioImageXD Project <http://www.bioimagexd.org/>"
+__version__ = "$Revision: 1.21 $"
+__date__ = "$Date: 2005/01/13 13:42:03 $"
+
+record = 0
+
+app = None
+mainwin = None
+
