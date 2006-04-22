e6c9c0b5ddc401218615d2d93547bf34788fd9d9
kalpaha <kalpaha@b2d525f8-dbc2-4032-a3a1-b86af67c733b>
2006-04-22 10:59:15 +0000

    Use streamer to split the dataset to smaller chunks
    git-svn-id: http://svn.code.sf.net/p/bioimagexd/code/home/svnuser/repos/selli/trunk@690 b2d525f8-dbc2-4032-a3a1-b86af67c733b

--- Tests/test_big.py
+++ Tests/test_big.py
@@ -3,6 +3,7 @@ import vtk
 import time
 D="/media/sda12/Data/selli/Selli_BIG.lsm"
 #D="/media/sda12/Data/selli/selli_coloc1_8-bit.lsm"
+#D="/media/sda12/Data/sample2.lsm"
 t=time.time()
 def elapsed():
     global t
@@ -16,25 +17,23 @@ r1.SetUpdateChannel(0)
 r2.SetUpdateChannel(1)
 d1=r1.GetOutput()
 d2=r2.GetOutput()
-print "Read data ",elapsed()
-
-
 
 itf1=vtk.vtkIntensityTransferFunction()
 itf2=vtk.vtkIntensityTransferFunction()
 
 ctf1=vtk.vtkColorTransferFunction()
 ctf1.AddRGBPoint(0,0,0,0)
-ctf1.AddRGBPoint(255.0,0,255,0)
+ctf1.AddRGBPoint(255.0,0,1.0,0)
 
 
 ctf2=vtk.vtkColorTransferFunction()
 ctf2.AddRGBPoint(0,0,0,0)
-ctf2.AddRGBPoint(255.0,255,0,0)
+ctf2.AddRGBPoint(255.0,1.0,0,0)
 
 
 print "Feeding channels to merge ",elapsed()
 merge = vtk.vtkImageColorMerge()
+#merge.SetNumberOfThreads(5)
 merge.AddInput(d1)
 merge.AddInput(d2)
 merge.AddLookupTable(ctf1)
@@ -42,14 +41,19 @@ merge.AddLookupTable(ctf2)
 merge.AddIntensityTransferFunction(itf1)
 merge.AddIntensityTransferFunction(itf2)
 
-merge.Update()
 
 mip=vtk.vtkImageSimpleMIP()
+#mip.DebugOn()
 print "Feeding merge to MIP",elapsed()
 mip.SetInput(merge.GetOutput())
-mip.ReleaseDataFlagOn()
+
+#streamer  = vtk.vtkMemoryLimitImageDataStreamer()
+#streamer.SetMemoryLimit(100*1024)
+#streamer.SetInput(mip.GetOutput())
+
 writer=vtk.vtkPNGWriter()
 writer.SetFileName("Selli_BIG.png")
+#writer.SetInput(streamer.GetOutput())
 writer.SetInput(mip.GetOutput())
 print "Feeding MIP to writer ",elapsed()
 writer.Update()
