3934e911cf7ba9341b1e9385862e0cbad019d47c
dan <dan@b2d525f8-dbc2-4032-a3a1-b86af67c733b>
2006-06-21 09:13:01 +0000

    change in build_app.py to comment out append for libguide.dylib intel compiler lib now have changed libguide.dylib install name to its actual path py2app should ber able to pick it up properly.
    git-svn-id: http://svn.code.sf.net/p/bioimagexd/code/home/svnuser/repos/selli/trunk@760 b2d525f8-dbc2-4032-a3a1-b86af67c733b

--- build_app.py
+++ build_app.py
@@ -113,15 +113,16 @@ def build():
             includes = incl_modules,
             packages = ["encodings"]
             )
-            DATA_FILES.append( ('../Frameworks', [
+            #DATA_FILES.append( ('../Frameworks', [
             #'/usr/local/lib/wxPython-unicode-2.5.5.1/lib/libwx_macud-2.5.5.rsrc',
             #appending the .rsrc file no longer needed in wx2.6
-            '/opt/intel/cc/9.1.027/lib/libguide.dylib',
+            #'/opt/intel/cc/9.1.027/lib/libguide.dylib',
             #this get the libguide.dylib into Frameworks but the app doenst see it.
             #sometihng to do with DLYD env vars set by intel script at 
             #/opt/intel/cc/9.1.024/bin/iccvars.sh which must be sourced before
             #using intel compilers, and before python2.4 BioImageXD.py works
-            ]))
+            #Maybe dont need this if libguide install name is set correctly to its acrualy path
+            #using install_name_tool -id old new]))
             setup(
                 app=['BioImageXD.py'],
                 data_files = DATA_FILES, 
