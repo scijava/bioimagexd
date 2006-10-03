391e8d8f2ae382c99aff1972787216ca0ad70b55
kalpaha <kalpaha@b2d525f8-dbc2-4032-a3a1-b86af67c733b>
2006-10-03 11:34:38 +0000

    Added ITK to libraries, added Abbi to contributors
    git-svn-id: http://svn.code.sf.net/p/bioimagexd/code/bioimagexd@834 b2d525f8-dbc2-4032-a3a1-b86af67c733b

--- trunk/GUI/AboutDialog.py
+++ trunk/GUI/AboutDialog.py
@@ -145,6 +145,7 @@ Jukka Varsaluoma<br>
 Jorma Virtanen<br>
 Mikko Huhtala<br>
 Pekka Neittaanmäki<br>
+Anna-Brita Öst<br>
 </p><p>
 <b>The following companies have supported the development of BioImageXD:</b></p><p>
 N / A
@@ -466,6 +467,7 @@ at the <i>Licensing</i> directory.</p>
 <ul>
 <li><a href="#python">Python</a></li>
 <li><a href="#vtk">The Visualization Toolkit</a></li>
+<li><a href="#itk">Insight Segmentation and Registration Toolkit</a></li>
 </ul>
 </p>
 <hr>
@@ -543,6 +545,46 @@ Redistribution and use in source and binary forms, with or without modification,
 </p><p>
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+</p>
+<hr>
+<p>
+<p><a name="itk"><h2>Insight Segmentation and Registration TOolkit</h2></a>
+</p>
+<p> 
+/*=========================================================================<br>
+<br>
+Copyright (c) 1999-2003 Insight Software Consortium<br>
+All rights reserved.<br>
+<br>
+Redistribution and use in source and binary forms, with or without<br>
+modification, are permitted provided that the following conditions are met:<br>
+<pre>
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * The name of the Insight Software Consortium, nor the names of any
+   consortium members, nor of any contributors, may be used to endorse or
+   promote products derived from this software without specific prior written
+   permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.</pre>
+</p><p>
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.<br>
+=========================================================================*/
 </p>
 </body>
 </html>
