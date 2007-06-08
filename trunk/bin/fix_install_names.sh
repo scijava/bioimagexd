ec428e0720d79932e1e7e7855f65224e2b5a1a3c
kalpaha <kalpaha@b2d525f8-dbc2-4032-a3a1-b86af67c733b>
2007-06-08 13:20:08 +0000

    Made the fix_install_names warn if the VTK directory specified is wrong
    git-svn-id: http://svn.code.sf.net/p/bioimagexd/code/bioimagexd@979 b2d525f8-dbc2-4032-a3a1-b86af67c733b

--- trunk/bin/fix_install_names.sh
+++ trunk/bin/fix_install_names.sh
@@ -1,7 +1,25 @@
 #! /bin/bash
-VTK_SOURCE_DIR="/Users/kallepahajoki/VTK/bin/"
-cd /Library/Frameworks/Python.framework/Versions/2.5/lib
 
+VTK_SOURCE_DIR="/Users/kallepahajoki/VTK"
+
+if [ "$1" != "" ]; then
+    VTK_SOURCE_DIR=$1
+fi
+
+if [ ! -d $VTK_SOURCE_DIR ]; then
+    echo "The VTK source directory specified does not exist"
+    echo "Current VTK directory: $VTK_SOURCE_DIRECTORY"
+    echo "You can either edit this script to change it permanently, or"
+    echo "Give it as an argument ot this script."
+    echo "Example:"
+    echo "# sudo sh fix_install_names.sh ~/VTK"
+    exit
+fi
+
+
+VTK_SOURCE_DIR="${VTK_SOURCE_DIR}/bin"
+
+cd /Library/Frameworks/Python.framework/Versions/2.5/lib
 
 LIBS="`echo *.dylib`"
 for lib in $LIBS
@@ -51,13 +69,13 @@ do
     DEPS="`otool -L  $lib |grep -v /|cut -d' ' -f1`"
     for dep in $DEPS
     do
-	install_name_tool -change $dep /Library/Frameworks/Python.framework/Versions/2.5/lib/$dep $lib
+	  install_name_tool -change $dep /Library/Frameworks/Python.framework/Versions/2.5/lib/$dep $lib
     done
     DEPS="`otool -L  $lib |grep $VTK_SOURCE_DIR|cut -d' ' -f1`"
     for dep in $DEPS
     do
-	ndep="`echo $dep | sed s,$VTK_SOURCE_DIR,,`"
-	install_name_tool -change $dep /Library/Frameworks/Python.framework/Versions/2.5/lib/$ndep $lib
+	  ndep="`echo $dep | sed s,$VTK_SOURCE_DIR,,`"
+	  install_name_tool -change $dep /Library/Frameworks/Python.framework/Versions/2.5/lib/$ndep $lib
     done
 
 done
