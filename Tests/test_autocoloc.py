105a05f2ce7dce1f75e51589d4e881a9df709d09
kalpaha <kalpaha@b2d525f8-dbc2-4032-a3a1-b86af67c733b>
2007-07-25 11:57:54 +0000

    Changes to the autothreshold test
    git-svn-id: http://svn.code.sf.net/p/bioimagexd/code/bioimagexd/trunk@1048 b2d525f8-dbc2-4032-a3a1-b86af67c733b

--- Tests/test_autocoloc.py
+++ Tests/test_autocoloc.py
@@ -1,33 +1,21 @@
 #! /usr/bin/env python
 import vtk
+import vtkbxd
 import sys
 sys.path.insert(0,"../lib")
 sys.path.insert(0,"..")
 import ImageOperations
-
+LSMFILE="/Users/kallepahajoki/BioImageXD/Data/sample1_single.lsm"
 reader=vtk.vtkLSMReader()
-reader.SetFileName("/home/kalpaha/BioImageXD/Data/sample2.lsm")
+reader.SetFileName(LSMFILE)
 reader.SetUpdateChannel(0)
-reader.SetUpdateTimePoint(5)
 reader.Update()
 ch1=reader.GetOutput()
 reader2=vtk.vtkLSMReader()
-reader2.SetFileName("/home/kalpaha/BioImageXD/Data/sample2.lsm")
+reader2.SetFileName(LSMFILE)
 reader2.SetUpdateChannel(1)
-reader2.SetUpdateTimePoint(5)
 reader2.Update()
 ch2=reader2.GetOutput()
-
-
-
-#w1=vtk.vtkXMLImageDataWriter()
-#w1.SetFileName("Ch1.vti")
-#w1.SetInput(ch1)
-#w1.Write()
-#w2=vtk.vtkXMLImageDataWriter()
-#w2.SetFileName("Ch2.vti")
-#w2.SetInput(ch2)
-#w2.Write()
 
 coloc=vtk.vtkImageAutoThresholdColocalization()
 coloc.AddInput(ch1)
@@ -36,39 +24,38 @@ coloc.Update()
 
 print coloc
 
-data=coloc.GetOutput(0)
-plotdata=coloc.GetOutput(1)
-print "Plot data=",plotdata
-i=0
-writer=vtk.vtkXMLImageDataWriter()
-pdata=data.GetPointData()
-name=None
-if pdata:
-    name=pdata.GetScalars().GetName()
-    if name:
-	name+=".vti"
-if not name:
-    name="unnamed.vti"%i
+#data=coloc.GetOutput(0)
+#plotdata=coloc.GetOutput(1)
+#print "Plot data=",plotdata
+#i=0
+#writer=vtk.vtkXMLImageDataWriter()
+#pdata=data.GetPointData()
+#name=None
+#if pdata:
+#    name=pdata.GetScalars().GetName()
+#    if name:#
+#	name+=".vti"
+#if not name:
+#    name="unnamed.vti"%i
+#
+#print "Writing",name,"..."
+#writer.SetFileName(name)
+#writer.SetInput(data)
+#writer.Write()
 
-print "Writing",name,"..."
-writer.SetFileName(name)
-writer.SetInput(data)
-writer.Write()
+#writer=vtk.vtkXMLImageDataWriter()
+#writer.SetInput(plotdata)
+#writer.SetFileName("scatterplot.vti")
+#writer.Write()
 
-writer=vtk.vtkXMLImageDataWriter()
-writer.SetInput(plotdata)
-writer.SetFileName("scatterplot.vti")
-writer.Write()
+#lut=ImageOperations.loadLUT("../LUT/Amber.lut")
 
-lut=ImageOperations.loadLUT("../LUT/Amber.lut")
+#maptocol=vtk.vtkImageMapToColors()
+#maptocol.SetLookupTable(lut)
+#maptocol.SetInput(plotdata)
+#maptocol.Update()
 
-maptocol=vtk.vtkImageMapToColors()
-maptocol.SetLookupTable(lut)
-maptocol.SetInput(plotdata)
-maptocol.Update()
-
-pngwriter=vtk.vtkPNGWriter()
-#pngwriter.SetInput(maptocol.GetOutput())
-pngwriter.SetInput(plotdata)
-pngwriter.SetFileName("scatterplot.png")
-pngwriter.Write()
+#pngwriter=vtk.vtkPNGWriter()
+#pngwriter.SetInput(plotdata)
+#pngwriter.SetFileName("scatterplot.png")
+#pngwriter.Write()
