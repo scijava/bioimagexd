b5c03df072296be190981a2f7eb6275cb7dfdfa4
kalpaha <kalpaha@b2d525f8-dbc2-4032-a3a1-b86af67c733b>
2007-08-11 12:43:38 +0000

    Add a script for creating the ITK-pkg directory from InsightToolkit source tree
    git-svn-id: http://svn.code.sf.net/p/bioimagexd/code/bioimagexd@1100 b2d525f8-dbc2-4032-a3a1-b86af67c733b

--- trunk/bin/create_itkpkg.sh
+++ trunk/bin/create_itkpkg.sh
@@ -1,1 +1,44 @@
-#! /bin/sh
+#! /bin/sh -x
+if [ -d "ITK-pkg" ]; then
+  	echo "ITK-pkg already exists"
+  	exit
+fi
+mkdir "ITK-pkg"
+cd ITK-pkg
+mkdir lib
+mkdir Python
+
+echo "Created ITK-pkg directory"
+for i in /c/BioImageXD/InsightToolkit/bin/*.dll
+do
+	TGT="`basename $i | sed s/dll/pyd/g`"
+	if [ "$TGT" = "ITKCommon.pyd" ]; then
+		TGT="ITKCommon.dll"
+	fi
+    cp $i lib/$TGT
+
+done
+for i in /c/BioImageXD/InsightToolkit/bin/*.py
+do
+	cp $i Python
+done
+
+cp /c/BioImageXD/InsightToolkit/Wrapping/WrapITK/Python/*.py Python
+
+cp -fr /c/BioImageXD/InsightToolkit/Wrapping/WrapITK/Python/itkExtras Python
+cp -fr /c/BioImageXD/InsightToolkit/Wrapping/WrapITK/Python/Configuration Python
+
+for extproj in ItkVtkGlue labelShape
+do
+	BASE="/c/BioImageXD/InsightToolkit/Wrapping/WrapITK/ExternalProjects/${extproj}"
+	cp $BASE/lib/*.py Python
+	for i in $BASE/lib/*.dll
+	do
+		TGT="`basename $i | sed s/dll/pyd/g`"
+		cp $i lib/$TGT
+	done
+
+
+	cp -fr $BASE/Python/Configuration/ Python
+	cp $BASE/Python/*.py Python
+done
