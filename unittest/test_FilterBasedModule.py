c09556ff4229ab3ab5aa66430f7fe9cb2d457adb
sseppola <sseppola@b2d525f8-dbc2-4032-a3a1-b86af67c733b>
2007-08-13 11:23:56 +0000

    * added test_FilterBasedModule, test_DataSource and test_command to unittest
    git-svn-id: http://svn.code.sf.net/p/bioimagexd/code/bioimagexd/trunk@1113 b2d525f8-dbc2-4032-a3a1-b86af67c733b

---
+++ unittest/test_FilterBasedModule.py
@@ -0,0 +1,106 @@
+# TestCase for lib.FilterBasedModule
+
+import unittest
+
+import lib.FilterBasedModule
+
+class FilterBasedModuleTest(unittest.TestCase):
+
+	def setUp(self):
+		"""
+		Created: 03.07.2007, MB
+		Description: Creating test data
+		"""
+		self.testFilterBasedModule = lib.FilterBasedModule.FilterBasedModule()	
+		
+		self.oldModifiedFlag = 0
+		self.newModifiedFlag = 24398238
+		self.testFilterBasedModule.modified = self.oldModifiedFlag
+
+		self.testCached = 1
+		self.testCachedTimepoint = 10213
+		self.testImages = ['lol', 'lol2']
+		self.testExtent = 1
+
+		self.testXSize = 1
+		self.testYSize = 2
+		self.testZSize = 3
+
+		self.testShift = 2
+		self.testScale = 43
+
+
+	def testSetModified(self):
+		"""
+		Created: 03.07.2007, MB
+		Description: A test for setModified()
+
+		A test checking if the parameters are flaged
+		"""
+
+		self.assertEqual(self.testFilterBasedModule.modified, self.oldModifiedFlag)
+		self.testFilterBasedModule.setModified(self.newModifiedFlag)
+		self.assertEqual(self.testFilterBasedModule.modified, self.newModifiedFlag)
+
+
+	def testReset(self):
+		"""
+		Created: 3.7.2007, MB
+		Description: Testing reset()
+
+		A test to check that the values get reseted
+		"""
+
+		self.testFilterBasedModule.cached = self.testCached
+		self.testFilterBasedModule.cachedTimepoint = self.testCachedTimepoint
+		self.testFilterBasedModule.images = self.testImages
+		self.testFilterBasedModule.extent = self.testExtent
+		self.testFilterBasedModule.xSize = self.testXSize
+		self.testFilterBasedModule.ySize = self.testYSize
+		self.testFilterBasedModule.zSize = self.testZSize
+		self.testFilterBasedModule.shift = self.testShift
+		self.testFilterBasedModule.scale = self.testScale
+
+		self.testFilterBasedModule.reset()
+		
+		self.assertEqual(self.testFilterBasedModule.cached, None)
+		self.assertEqual(self.testFilterBasedModule.cachedTimepoint, -1)
+		self.assertEqual(self.testFilterBasedModule.images, [])
+		self.assertEqual(self.testFilterBasedModule.extent, None)
+		self.assertEqual(self.testFilterBasedModule.xSize, 0)
+		self.assertEqual(self.testFilterBasedModule.ySize, 0)
+		self.assertEqual(self.testFilterBasedModule.zSize, 0)
+		self.assertEqual(self.testFilterBasedModule.shift, 0)
+		self.assertEqual(self.testFilterBasedModule.scale, 1)
+
+
+	def testAddInput(self):
+		"""
+		Created:
+		Description:
+		"""
+
+		pass
+
+
+	def testGetPreview(self):
+		"""
+		Created:
+		Description
+		"""
+
+		pass
+
+
+	def testDoOperation(self):
+		"""
+		Created:
+		Description:
+		"""
+
+		pass
+
+
+#run
+if __name__ == "__main__":
+	unittest.main()
---
+++ unittest/test_command.py
@@ -0,0 +1,88 @@
+# TestCase for lib.Command
+
+import unittest
+import lib.Command
+
+class CommandTest(unittest.TestCase):
+	
+	def setUp(self):
+		"""
+		Description: Creating a test Command and other test data
+		"""
+		self.testUndocmd = "Test UndoCommand"
+		self.testCategory = "Test Category"
+		self.testDesc = "Test Description"
+		self.testCommand = lib.Command.Command(self.testCategory, None, None, \
+							"", "", None, self.testDesc)
+		self.testImports = []
+		self.testCode = "I\nam\na\npiece\nof\ntest\ncode"
+		self.testCodeEmpty = ""
+		self.test_Undoed = 1
+
+		# in Command.py functionize() adds following string before codestrings
+		self.functionizeString = "    "
+
+	def tearDown(self):
+		pass
+
+	def testFunctionizeEmptyValues(self):
+		"""
+		Description: A test for the functionize() method
+		Tests that functionize modifies the inputtext, even with empty values
+		"""
+		testCodeModified = self.testCommand.functionize(self.testCodeEmpty, self.testImports)
+		self.assertNotEqual(self.testCodeEmpty, testCodeModified, msg = "Original input equals modified")
+		
+	def testFunctionize(self):
+		"""
+		Description: A test for the functionize() method
+		Tests that functionize modifies the text correctly
+		Every line in modifiedLines[i] should be equal to ("    " + originalLines[i])
+		"""
+		testCodeModified = self.testCommand.functionize(self.testCode, self.testImports)
+		modifiedLines = testCodeModified.split("\n")
+		originalLines = self.testCode.split("\n")
+
+		for i, line in enumerate(originalLines):
+			self.assertEqual(modifiedLines[i], (self.functionizeString + originalLines[i]))
+
+	def testGetDesc(self):
+		"""
+		Description: A test for the getDesc() method
+		Checks that getDesc() returns the "desc" input value of a Command object
+		"""
+		self.assertEqual(self.testDesc, self.testCommand.getDesc())
+
+	def testIsUndoed(self):
+		"""
+		Description: A test for the isUndoed() method
+		Tests that isUndoed() returns the correct "_undoed" value
+		"""
+		self.testCommand._undoed = self.test_Undoed
+		self.assertEqual(self.testCommand.isUndoed(), self.test_Undoed)
+
+	def testCanUndo(self):
+		"""
+		Description: A test for the CanUndo() method
+		Tests that the output is true or false
+		"""
+		self.assertEqual(self.testCommand.canUndo(), bool(self.testCommand.undocmd))
+
+	def testRun(self):
+
+		pass
+
+	def testUndo(self):
+
+		pass
+
+	def testGetCategory(self):
+		"""
+		Description: A test for the getCategory() method
+		Tests that getCategory() returns the "category" input value of a Command object
+		"""
+		self.assertEqual(self.testCategory, self.testCommand.getCategory())
+
+#run
+if __name__ == "__main__":
+	unittest.main()
