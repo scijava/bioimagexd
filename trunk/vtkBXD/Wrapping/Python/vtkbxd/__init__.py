98affc6c5f18f5d842328119448bd5dcb0f63e2b
twikstro <twikstro@b2d525f8-dbc2-4032-a3a1-b86af67c733b>
2007-08-14 08:17:56 +0000

    * Removed unused imports * Some other minor corrections
    git-svn-id: http://svn.code.sf.net/p/bioimagexd/code/bioimagexd@1119 b2d525f8-dbc2-4032-a3a1-b86af67c733b

--- trunk/vtkBXD/Wrapping/Python/vtkbxd/__init__.py
+++ trunk/vtkBXD/Wrapping/Python/vtkbxd/__init__.py
@@ -1,7 +1,7 @@
 #import vtkbxd
 #import vtk
-from vtkbxd import *
+#from vtkbxd import *
 #
 #for key,val in vtkbxd.__dict__.items():
-#    if key not in vtk.__dict__:
-#        vtk.__dict__[key]=vtkbxd.__dict__[key]
+#	if key not in vtk.__dict__:
+#		vtk.__dict__[key]=vtkbxd.__dict__[key]
