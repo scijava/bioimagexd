e38ec0b40dba12f5fda0dfb7b4e4a9b53afb763a
kalpaha <kalpaha@b2d525f8-dbc2-4032-a3a1-b86af67c733b>
2007-08-15 05:25:43 +0000

    removed the unnecessary vtkbxd.py
    git-svn-id: http://svn.code.sf.net/p/bioimagexd/code/bioimagexd/trunk@1124 b2d525f8-dbc2-4032-a3a1-b86af67c733b

--- vtkBXD/Wrapping/Python/vtkbxd/vtkbxd.py
+++
@@ -1,9 +0,0 @@
-""" This module loads all the classes from the BioImageXD Processing Imaging library into
-its namespace.  This is a required module."""
-
-import os
-
-if os.name == 'posix':
-    from libvtkBXDProcessingPython import *
-else:
-    from vtkBXDProcessingPython import *
