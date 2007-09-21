75ee9cf1fd9cd065efa32054948bbd4dad1b1526
kalpaha <kalpaha@b2d525f8-dbc2-4032-a3a1-b86af67c733b>
2007-09-21 18:09:09 +0000

    git-svn-id: http://svn.code.sf.net/p/bioimagexd/code/bioimagexd@1198 b2d525f8-dbc2-4032-a3a1-b86af67c733b

--- trunk/bin/build_installer.sh
+++ trunk/bin/build_installer.sh
@@ -1,3 +1,11 @@
 #! /bin/sh
-/bin/sh bin/create_iss.sh
-"/cygdrive/c/Program Files/Inno Setup 5/iscc" BioImageXD.iss
+if [ "$1" = "" ]; then
+   echo "Usage: build_installer.sh <svn revision>"
+   exit
+fi
+TGT=BioImageXD-beta-r$1.exe
+#/bin/sh bin/create_iss.sh
+#"/cygdrive/c/Program Files/Inno Setup 5/iscc" BioImageXD.iss
+mv /cygdrive/c/Temp/Setup.exe /cygdrive/c/temp/${TGT}
+echo curl -T "C:\\Temp\\${TGT}" -u anonymous:kalle.pahajoki@gmail.com ftp://upload.sourceforge.net/incoming/
+
