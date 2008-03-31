99d0620e1fce983beafa5438fc841dacc9e61db3
kalpaha <kalpaha@b2d525f8-dbc2-4032-a3a1-b86af67c733b>
2008-03-31 07:48:24 +0000

    Add the coloc list view
    git-svn-id: http://svn.code.sf.net/p/bioimagexd/code/bioimagexd@1409 b2d525f8-dbc2-4032-a3a1-b86af67c733b

---
+++ trunk/bin/copy_to_sourceforge.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+if [ ! -d ../sourceforge ]; then
+	echo "The directory ../sourceforge does not exist"
+	echo "Check out the sourceforge repository before running this script"
+	echo "Command for checking out the SVN:"
+	echo 
+	echo "svn co https://bioimagexd.svn.sourceforge.net/svnroot/bioimagexd/bioimagexd/trunk ../sourceforge"
+	exit
+fi
+svn update
+rm -fr ../clean-exp
+echo "Exporting repository to clean directory"
+svn export . ../clean-exp
+cd ..
+
+echo "Copying files from clean export to sourceforge"
+find clean-exp -type f | while read file
+do
+  cp "$file" "`echo $file|sed s/clean-exp/sourceforge/`"
+done 
+#echo "Remember to set the t-flag"
+echo "Modifying T-flag"
+perl -i -p -e 's/TFLag = 0/TFLag = 1/g' sourceforge/scripting.py
+cd sourceforge
+echo
+echo "Possibly modified files:"
+svn stat|grep "^?"|grep -v ".pyc$"
+
+echo "Committing to sourceforge"
+svn commit -m "Update from internal repository to sourceforge"
