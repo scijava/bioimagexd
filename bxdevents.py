88e0fa641ed4a1516c13ec0ede05cc4193cebd2f
lopaavol <lopaavol@b2d525f8-dbc2-4032-a3a1-b86af67c733b>
2008-11-20 09:47:59 +0000

    Added bxdevents.py
    git-svn-id: http://svn.code.sf.net/p/bioimagexd/code/bioimagexd/trunk@1459 b2d525f8-dbc2-4032-a3a1-b86af67c733b

---
+++ bxdevents.py
@@ -0,0 +1,29 @@
+"""
+ Module: bxdevents
+
+ A module containing constants that define the messages used in passing messages inside the software
+  
+ Copyright (C) 2008  BioImageXD Project
+ See CREDITS.txt for details
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""
+__author__ = "BioImageXD Project"
+__version__ = "$Revision: 1.21 $"
+__date__ = "$Date: 2005/01/13 13:42:03 $"
+
+DATA_DIMENSIONS_CHANGED="Notification: the dimensions of the viewed data have changed"
+TRANSLATE_DATA="Notification: the image data has been translated by the given amount"
