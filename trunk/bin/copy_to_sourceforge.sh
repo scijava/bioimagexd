09d7aeea211753f7ec24bb064879e9017ee44be7
lopaavol <lopaavol@b2d525f8-dbc2-4032-a3a1-b86af67c733b>
2009-06-16 09:55:08 +0000

    Update from internal repository to sourceforge
    git-svn-id: http://svn.code.sf.net/p/bioimagexd/code/bioimagexd@1514 b2d525f8-dbc2-4032-a3a1-b86af67c733b

--- trunk/bin/copy_to_sourceforge.sh
+++ trunk/bin/copy_to_sourceforge.sh
@@ -37,7 +37,7 @@ echo "Possibly modified files:"
 svn stat|grep "^?"|grep -v ".pyc$"
 
 echo "Committing to sourceforge"
-if [$1]; then
+if [-n "$1"]; then
 	svn commit -m "$1"
 else
 	svn commit -m "Update from internal repository to sourceforge"
