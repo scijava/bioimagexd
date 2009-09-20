1feae6491264a4dbf26100ef984f7289e02bfa30
lopaavol <lopaavol@b2d525f8-dbc2-4032-a3a1-b86af67c733b>
2009-09-20 17:44:49 +0000

    Added comments in package creation scripts.
    git-svn-id: http://svn.code.sf.net/p/bioimagexd/code/bioimagexd@1557 b2d525f8-dbc2-4032-a3a1-b86af67c733b

--- trunk/bin/create_itkpkg.sh
+++ trunk/bin/create_itkpkg.sh
@@ -1,9 +1,12 @@
 #! /bin/sh -x
+
+# This script should be run from the main BioImageXD directory
+
 if [ -d "ITK-pkg" ]; then
   	echo "ITK-pkg already exists"
   	exit
 fi
-mkdir "ITK-pkg"
+mkdir "ITK-pkg" # Makes directory ITK-pkg where ITK files will be copied
 cd ITK-pkg
 mkdir lib
 mkdir lib/Release
@@ -13,6 +16,9 @@ mkdir Python/Configuration
 mkdir Python/Release/itkExtras
 
 echo "Created ITK-pkg directory"
+
+# Next we copy all dlls from the directory where ITK was build to
+# ITK-pkg/lib/Release and rename those by pyd file type, except two exceptions
 for i in /cygdrive/c/BioImageXD/ITK/bin/Release/*.dll
 do
 	TGT="`basename $i | sed s/dll/pyd/g`"
@@ -25,51 +31,52 @@ do
     cp $i lib/Release/$TGT
 done
 
+# Next copy every pyd file from the directory where ITK was build
 cp /cygdrive/c/BioImageXD/ITK/bin/Release/*.pyd lib/Release
+# Copy specific manifest file from BioImageXD/bin directory
 cp /cygdrive/c/BioImageXD/trunk/bin/Microsoft.VC90.CRT.ITK.manifest lib/Release/Microsoft.VC90.CRT.manifest
-#cp /cygdrive/c/BioImageXD/InsightToolkit/lib/Release/*.pyd lib
 
-
+# Then we copy all py files from the ITK installation directory created by
+# WrapITK. These files include all python modules for ITK classes
+# (example file itkThresholdImageFilter.py)
 cp /cygdrive/c/BioImageXD/ITK/bin/Release/*.py Python/Release
+# Copy all py files installed in ITK/Wrapping/WrapITK.
+# These files include different ITK modules like Filtering.py and Review.py
 cp /cygdrive/c/BioImageXD/ITK/Wrapping/WrapITK/Python/Release/*.py Python/Release
 
+# Then copy rest of the stuff build when ITK was wrapped. This is probably not
+# even needed but copy it to be sure.
 cp /cygdrive/c/BioImageXD/ITK/Wrapping/WrapITK/Python/itkExtras/__init__.py Python/Release/itkExtras
+# Copy configuration files of Python wrapping like FilteringConfig.py.
+# These as well are not even needed. But haven't tested without those.
 cp -fr /cygdrive/c/BioImageXD/ITK/Wrapping/WrapITK/Python/Configuration Python
 
-#cp /cygdrive/c/BioImageXD/InsightToolkit/lib/Release/*.py Python
-#cp /cygdrive/c/BioImageXD/InsightToolkit/Python/Release/*.py Python
-#cp /cygdrive/c/BioImageXD/InsightToolkit/Python/Configuration/*.py Python
-
+# Copy ItkVtkGlue and labelShape external projects. BASE here defines base
+# directory that includes both projects. Copy all py, pyd and configuration
+# files. If these are originally build where ITK is also build then these
+# steps can be skipped as all of these files are already copied when ITK
+# was copied.
 for extproj in ItkVtkGlue labelShape
 do
 	BASE="/cygdrive/c/BioImageXD/InsightToolkit-3.14.0/Wrapping/WrapITK/ExternalProjects/${extproj}"
 	cp $BASE/lib/Release/*.py Python/Release
 	cp $BASE/lib/Release/*.pyd lib/Release
-	#for i in $BASE/lib/Release/*.dll
-	#do
-	#	TGT="`basename $i | sed s/dll/pyd/g`"
-	#	cp $BASE/lib/Release/$TGT lib/Release/$TGT
-	#done
-
 	cp -fr $BASE/Python/Configuration/ Python
 	cp $BASE/Python/Release/*.py Python/Release
 done
 
+# Do same as earlier for external projects to itkBXD
 for extproj in itkBXD
 do
 	BASE="/cygdrive/c/BioImageXD/trunk/${extproj}"
 	cp $BASE/lib/Release/*.py Python/Release
 	cp $BASE/lib/Release/*.pyd lib/Release
-	#for i in $BASE/lib/Release/*.dll
-	#do
-	#	TGT="`basename $i | sed s/dll/pyd/g`"
-	#	cp $BASE/lib/Release/$TGT lib/Release/$TGT
-	#done
-
 	cp -fr $BASE/Python/Configuration/ Python
 	cp $BASE/Python/Release/*.py Python/Release
 done
 
+# Change rights in Cygwin that it is possible to make a package out of these
+# files.
 chmod 644 lib/*
 chmod 755 lib/Release/*
 chmod 644 Python/*
